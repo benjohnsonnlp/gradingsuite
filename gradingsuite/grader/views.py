875aa290d7415e31eab4c9d9c612601cc1b55bbf
Ben <doc.ben.johnson@gmail.com>
2019-05-30 17:48:01 -0400

    first template for grading

--- gradingsuite/grader/views.py
+++ gradingsuite/grader/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Yo.")
+    template = loader.get_template("grader/index.html")
+    return HttpResponse(template.render({}, request))
