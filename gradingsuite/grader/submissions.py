d55fbd4752d08d1c08967d3ae11d182b3bfe46e0
Ben <doc.ben.johnson@gmail.com>
2019-05-30 18:51:07 -0400

    Lists of submissions

--- gradingsuite/grader/submissions.py
+++ gradingsuite/grader/submissions.py
@@ -5,11 +5,15 @@ class Submission:
     def __init__(self, filename):
         self.filename = filename
 
+    def __str__(self):
+        return self.filename
+
 
 def get_submissions(filename):
     submissions = []
-    for subdir, dirs, files in os.walk(filename):
-        for file in files:
-            submissions.append(Submission(os.path.join(subdir, file)))
+    for subdir in os.listdir(filename):
+        full_subdir = os.path.join(filename, subdir)
+        if os.path.isdir(full_subdir):
+            submissions.append(Submission(full_subdir))
 
     return submissions
--- gradingsuite/grader/views.py
+++ gradingsuite/grader/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.template import loader
 
 from grader.models import Assignment
+from grader.submissions import get_submissions
 
 
 def index(request):
@@ -16,7 +17,7 @@ def index(request):
 def assignment(request, assignment_id):
     template = loader.get_template("grader/assignment.html")
     assignment = get_object_or_404(Assignment, pk=assignment_id)
-    submissions = []
+    submissions = get_submissions(assignment.home_dir)
     context = {
         "assignment": assignment,
         "submissions": submissions,
