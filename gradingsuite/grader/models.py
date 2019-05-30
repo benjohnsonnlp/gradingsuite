96e8c5f3b89294a70261d1105811200aeb042bd5
Ben <doc.ben.johnson@gmail.com>
2019-05-30 18:09:03 -0400

    scrubbing source strings

--- gradingsuite/grader/models.py
+++ gradingsuite/grader/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=200)
+    home_dir = models.CharField(max_length=1000)
--- gradingsuite/grader/views.py
+++ gradingsuite/grader/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from grader.models import Assignment
+
 
 def index(request):
     template = loader.get_template("grader/index.html")
-    return HttpResponse(template.render({}, request))
+    context = {
+        "assignments": Assignment.objects.all(),
+    }
+    return HttpResponse(template.render(context, request))
