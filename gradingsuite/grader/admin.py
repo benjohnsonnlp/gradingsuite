31d8e090335e1addedccda9884ee15df873de876
Ben <doc.ben.johnson@gmail.com>
2019-06-01 03:42:37 -0400

    rubric model first pass + README

--- gradingsuite/grader/admin.py
+++ gradingsuite/grader/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from grader.models import Assignment
+from grader.models import *
 
 admin.site.register(Assignment)
+admin.site.register(Rubric)
+admin.site.register(RubricSection)
+admin.site.register(RubricItem)
--- gradingsuite/grader/models.py
+++ gradingsuite/grader/models.py
@@ -5,3 +5,20 @@ class Assignment(models.Model):
     name = models.CharField(max_length=200)
     home_dir = models.CharField(max_length=1000, default="/")
     language = models.CharField(max_length=200, default="python")
+
+
+class Rubric(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=200, default="")
+
+
+class RubricSection(models.Model):
+    rubric = models.ForeignKey(Rubric, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000, default="General")
+
+
+class RubricItem(models.Model):
+    section = models.ForeignKey(RubricSection, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000, default="")
+    possible_points = models.IntegerField()
+    earned_points = models.IntegerField()
