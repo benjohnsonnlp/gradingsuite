03577d6dc0f4d7eb6bb32cdf65490ebcf074b22f
Ben <doc.ben.johnson@gmail.com>
2019-06-05 13:32:41 -0400

    static thing

--- gradingsuite/gradingsuite/settings.py
+++ gradingsuite/gradingsuite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
