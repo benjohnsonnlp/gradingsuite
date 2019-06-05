55028eb8857019046b5c1df5bcfcdf7d9378782f
Ben <doc.ben.johnson@gmail.com>
2019-06-05 13:34:01 -0400

    remove static dirs

--- gradingsuite/gradingsuite/settings.py
+++ gradingsuite/gradingsuite/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
